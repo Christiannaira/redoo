d4ce0476b14b73566f537e9c0083230af5a167f2
Christian <Christianmnaira@gmail.com>
2025-11-16 14:15:46 +0800

    fix: able to make the signup content responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,21 @@ function App() {
    return (
       <div>
          <SignUp />
+
+         {/* <div className="p-5">
+            <h1>Tailwind Practice</h1>
+            <div className="border max-w-[200px] w-full">
+               <h2>Content inside</h2>
+               <input
+                  type="text"
+                  placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+                  ab voluptate facere sint officiis repudiandae debitis enim qui
+                  ullam, consequatur, perferendis alias harum! Labore suscipit
+                  cupiditate numquam totam exercitationem quasi?"
+                  className="max-w-full w-[200px]"
+               />
+            </div>
+         </div> */}
       </div>
    );
 }
--- src/components/SigningCredentials/SignUp.jsx
+++ src/components/SigningCredentials/SignUp.jsx
@@ -2,123 +2,145 @@ import { PiUserDuotone, PiEye, PiEyeClosed } from "react-icons/pi";
 import { TfiEmail } from "react-icons/tfi";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { useState } from "react";
+import { GoArrowLeft } from "react-icons/go";
 
 const SignUp = () => {
-   // Password visibility state
-   const [passOpen, setPassOpen] = useState(true);
-   const [secondPassOpen, setSecondPassOpen] = useState(true);
+   const [passOpen, setPassOpen] = useState(false);
+   const [secondPassOpen, setSecondPassOpen] = useState(false);
 
-   // Form input states
    const [username, setUsername] = useState("");
    const [email, setEmail] = useState("");
    const [password, setPassword] = useState("");
    const [confirmPassword, setConfirmPassword] = useState("");
 
-   const openFirstPass = () => {
-      setPassOpen(!passOpen);
-   };
-
-   const openSecondPass = () => {
-      setSecondPassOpen(!secondPassOpen);
-   };
-
    const handleSignUp = (e) => {
       e.preventDefault();
 
       const newEntry = {
-         username: username,
-         email: email,
-         password: password,
-         confirmPassword: confirmPassword,
+         username,
+         email,
+         password,
+         confirmPassword,
       };
 
       console.log(newEntry);
    };
 
    return (
-      <div className="max-w-full h-screen p-5 box-border">
-         <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 h-full">
-            <div className="place-content-center place-items-center border">
-               <form>
-                  <div className="mb-3">
-                     <h2 className="text-blue-500 text-5xl mb-2">Sign Up</h2>
-                     <p>Manage Library Books With Ease</p>
+      <div className="w-full min-h-screen p-5 grid lg:grid-cols-2 grid-cols-1">
+         {/* LEFT SIDE FORM */}
+         <div className="flex justify-around items-center flex-col">
+            <div>
+               <div></div>
+               <h4>
+                  Already an admin member? <a href="#">Sign In</a>
+               </h4>
+            </div>
+            <form
+               onSubmit={handleSignUp}
+               className="w-full max-w-[500px]"
+               id="signupForm"
+            >
+               <div>
+                  <div className="mb-5">
+                     <h2 className="text-[#FF6927] text-5xl mb-2 font-semibold">
+                        Sign Up
+                     </h2>
+                     <p className="text-gray-600">
+                        Manage Library Books With Ease
+                     </p>
                   </div>
-                  <div className="flex bg-gray-100 w-full max-w-[500px] p-3 rounded-sm justify-start items-center gap-3 mb-3">
-                     <PiUserDuotone size={20} color="#737373" />
+
+                  {/* USERNAME FIELD */}
+                  <div className="flex items-center gap-3 bg-gray-100 p-3 rounded mb-4 border border-gray-300">
+                     <PiUserDuotone size={22} className="text-gray-500" />
                      <input
                         type="text"
                         placeholder="Enter Your Username"
-                        className="max-w-[420px] focus:outline-none focus:ring-0"
+                        className="flex-1 min-w-0 bg-transparent focus:outline-none"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                      />
                   </div>
-                  <div className="flex bg-gray-100 w-full max-w-[500px] p-3 rounded-sm justify-start items-center gap-3 mb-3">
-                     <TfiEmail size={20} color="#737373" />
+
+                  {/* EMAIL FIELD */}
+                  <div className="flex items-center gap-3 bg-gray-100 p-3 rounded mb-4 border border-gray-300">
+                     <TfiEmail size={20} className="text-gray-500" />
                      <input
                         type="email"
                         placeholder="Enter Your Email"
-                        className="max-w-[420px] focus:outline-none focus:ring-0"
+                        className="flex-1 min-w-0 bg-transparent focus:outline-none"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                      />
                   </div>
-                  <div className="flex bg-gray-100 w-full max-w-[500px] p-3 rounded-sm justify-start items-center gap-3 mb-3">
-                     <RiLockPasswordLine size={25} color="#737373" />
+
+                  {/* PASSWORD FIELD */}
+                  <div className="flex items-center gap-3 bg-gray-100 p-3 rounded mb-4 border border-gray-300">
+                     <RiLockPasswordLine size={24} className="text-gray-500" />
                      <input
                         type={passOpen ? "text" : "password"}
                         placeholder="Enter Your Password"
-                        className="max-w-[380px] focus:outline-none focus:ring-0"
+                        className="flex-1 min-w-0 bg-transparent focus:outline-none"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                      />
-                     <PiEye
-                        size={25}
-                        color="#737373"
-                        onClick={openFirstPass}
-                        className={passOpen ? "hidden" : ""}
-                     />
-                     <PiEyeClosed
-                        size={25}
-                        color="#737373"
-                        className={passOpen ? "" : "hidden"}
-                        onClick={openFirstPass}
-                     />
+
+                     {passOpen ? (
+                        <PiEyeClosed
+                           size={24}
+                           onClick={() => setPassOpen(false)}
+                           className="text-gray-600 cursor-pointer"
+                        />
+                     ) : (
+                        <PiEye
+                           size={24}
+                           onClick={() => setPassOpen(true)}
+                           className="text-gray-600 cursor-pointer"
+                        />
+                     )}
                   </div>
-                  <div className="flex bg-gray-100 w-[500px] p-3 rounded-sm justify-start items-center gap-3 mb-3">
-                     <RiLockPasswordLine size={25} color="#737373" />
+
+                  {/* CONFIRM PASSWORD FIELD */}
+                  <div className="flex items-center gap-3 bg-gray-100 p-3 rounded mb-4 border border-gray-300">
+                     <RiLockPasswordLine size={24} className="text-gray-500" />
                      <input
                         type={secondPassOpen ? "text" : "password"}
                         placeholder="Confirm Your Password"
-                        className="w-[380px] focus:outline-none focus:ring-0"
+                        className="flex-1 min-w-0 bg-transparent focus:outline-none"
                         value={confirmPassword}
                         onChange={(e) => setConfirmPassword(e.target.value)}
                      />
-                     <PiEye
-                        size={25}
-                        color="#737373"
-                        onClick={openSecondPass}
-                        className={secondPassOpen ? "hidden" : ""}
-                     />
-                     <PiEyeClosed
-                        size={25}
-                        color="#737373"
-                        className={secondPassOpen ? "" : "hidden"}
-                        onClick={openSecondPass}
-                     />
+
+                     {secondPassOpen ? (
+                        <PiEyeClosed
+                           size={24}
+                           onClick={() => setSecondPassOpen(false)}
+                           className="text-gray-600 cursor-pointer"
+                        />
+                     ) : (
+                        <PiEye
+                           size={24}
+                           onClick={() => setSecondPassOpen(true)}
+                           className="text-gray-600 cursor-pointer"
+                        />
+                     )}
                   </div>
-                  <div>
-                     <button
-                        onClick={handleSignUp}
-                        className="bg-[#FF6927] text-white p-3 px-10 text-xl rounded-full"
-                     >
-                        Sign Up
-                     </button>
-                  </div>
-               </form>
-            </div>
-            <div>hello</div>
+               </div>
+               {/* BUTTON */}
+            </form>
+            <button
+               type="submit"
+               className="bg-[#FF6927] hover:bg-[#ff7f45] transition text-white p-3 px-10 text-xl rounded-full w-50"
+               form="signupForm"
+            >
+               Sign Up
+            </button>
+         </div>
+
+         {/* RIGHT SIDE */}
+         <div className="flex justify-center items-center">
+            <p className="text-gray-400 text-xl">hello</p>
          </div>
       </div>
    );
