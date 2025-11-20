3795dca58d5291f1f93ff7b26d9f7db3fe3fbb74
Christian <Christianmnaira@gmail.com>
2025-11-20 13:37:06 +0800

    feat: added a userprofilecard component that display all user details

--- src/components/homepage/sidebar/cards/UserCard.jsx
+++ src/components/homepage/sidebar/cards/UserCard.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const UserCard = ({ user }) => {
    const dateCreation = new Date(user.dateCreated);
 
+   const navigator = useNavigate();
+
+   const handleUser = (user) => {
+      alert(user.username);
+      navigator("/userprofile", { state: user });
+   };
+
    return (
-      <div className="flex justify-between items-center bg-gray-200 mb-2 py-2 px-3 rounded-sm hover:bg-[#FF6927] cursor-pointer transition-full duration-200 ease-in-out">
+      <div
+         className="flex justify-between items-center bg-gray-200 mb-2 py-2 px-3 rounded-sm hover:bg-[#FF6927] cursor-pointer transition-full duration-200 ease-in-out"
+         onClick={() => handleUser(user)}
+      >
          <h3>{user.username}</h3>
          <span>
             {dateCreation.toLocaleDateString("en-US", {
--- src/components/homepage/sidebar/cards/UserProfileCard.jsx
+++ src/components/homepage/sidebar/cards/UserProfileCard.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const UserProfileCard = () => {
-   return <div>UserProfileCard</div>;
+   const location = useLocation();
+   const user = location.state;
+
+   return (
+      <div>
+         UserProfileCard
+         <h3>{user.username}</h3>
+      </div>
+   );
 };
 
 export default UserProfileCard;
