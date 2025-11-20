4318a8b25b1a24338f332af8b8df20465c989003
Christian <Christianmnaira@gmail.com>
2025-11-20 12:05:35 +0800

    feat: added get all users services

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -4,3 +4,5 @@ const REST_API_BASE_URL = "http://localhost:8080/users";
 
 
 export const createUser = (user) => axios.post(REST_API_BASE_URL, user);
+
+export const listUsers = () => axios.get(REST_API_BASE_URL);
