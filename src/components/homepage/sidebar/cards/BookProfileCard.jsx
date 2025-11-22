e46fa7ede30cd393502c5d04ce76f079ceee2602
Christian <Christianmnaira@gmail.com>
2025-11-22 16:29:28 +0800

    feat: implemented the get book by id

--- src/components/homepage/sidebar/cards/BookProfileCard.jsx
+++ src/components/homepage/sidebar/cards/BookProfileCard.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getBook } from "../../../../services/BooksServices";
 
 const BookProfileCard = () => {
+   const { id } = useParams();
+   const [book, setBook] = useState(null);
    const navigate = useNavigate();
+
+   useEffect(() => {
+      getBook(id)
+         .then((response) => setBook(response.data))
+         .catch((err) => console.error(err));
+   }, [id]);
+
+   if (!book) return <p>Loading...</p>;
 
    return (
       <div>
+         <h3>{book.title}</h3>
+         <h3>{book.author}</h3>
          <a onClick={() => navigate("..")}>go back</a>
       </div>
    );
--- src/services/BooksServices.js
+++ src/services/BooksServices.js
@@ -4,4 +4,6 @@ const REST_API_BASE_URL = "http://localhost:8080/books";
 
 export const listBooks = () => axios.get(REST_API_BASE_URL);
 
+export const getBook = (bookId) => axios.get(REST_API_BASE_URL + "/" + bookId);
+
 export const bookCount = () => axios.get(REST_API_BASE_URL + "/count");
